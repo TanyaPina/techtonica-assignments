355aae778b89c04fe7b5aec3b25b26ca639b508f
Tanya Pina <tanyapina15@gmail.com>
2023-02-03 18:05:49 -0800

    updated submit event

--- personal portfolio/portfolio.js
+++ personal portfolio/portfolio.js
@@ -1,21 +1,8 @@
 
-const userInput = document.getElementById("name")
-
-//element.addEventListener("submit", (event) => {
- //   event.preventDefault();
- //   console.log(userInput.value);
-//})
+//const userInput = document.getElementById("name")
 const form = document.getElementById("contact-me-here");
 const welcomeText = document.getElementById("welcome");
-/*
-element.addEventListener("submit", function () {
-    
-    document.getElementById("welcome").innerHTML = "Hello!";
-});
-//function formFunction(){
-//    document.getElementById("welcome").innerHTML =  "Hello!";
-//}
-*/
+
 
 function onSubmit(event) {
     welcomeText.innerHTML = "Thanks for visiting!"
