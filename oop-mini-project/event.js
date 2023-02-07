dbe9990da7cedd0916abdbc408ebf9f18d855e80
Tanya Pina <tanyapina15@gmail.com>
2023-02-07 12:57:04 -0800

    added new event objects

--- oop-mini-project/event.js
+++ oop-mini-project/event.js
@@ -5,3 +5,14 @@ class Event {
       this.availableTickets = [];
     }
   }
+
+  const eventObj1 = new Event(
+    'KLOS Golden Gala',
+    'An evening with hollywood vampires'
+  );
+
+  const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
+  const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+
+  console.log(eventObj2);
+  console.log(eventObj3);
