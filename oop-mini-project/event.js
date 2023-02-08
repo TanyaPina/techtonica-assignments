9bc16866e4371e7e31f935b847943fb10d077352
Tanya Pina <tanyapina15@gmail.com>
2023-02-07 20:15:02 -0800

    added additional ticket types

--- oop-mini-project/event.js
+++ oop-mini-project/event.js
@@ -3,6 +3,10 @@ class Event {
       this.name = name;
       this.description = description;
       this.availableTickets = [];
+      this.addAvailableTickets = function (ticketType, price) {
+        this.availableTickets = ticketType,
+        this.price = price;
+      }
     }
   }
 
@@ -32,9 +36,24 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#event').innerHTML = html;
   });
 
-class TicketType {
+class ticketType {
     constructor(name, price) {
         this.name = name;
         this.price = price;
     }
+
 }
+eventObj1.addAvailableTickets("human", 299);
+console.log(eventObj1);
+eventObj1.addAvailableTickets("vampire", 99);
+console.log(eventObj1);
+eventObj2.addAvailableTickets("General Admission", 25)
+console.log(eventObj2);
+eventObj2.addAvailableTickets("Floor Seating", 80)
+console.log(eventObj2);
+eventObj3.addAvailableTickets("Orchestra", 300)
+console.log(eventObj3);
+eventObj3.addAvailableTickets("Mezzanine", 200)
+console.log(eventObj3);
+eventObj3.addAvailableTickets("Balcony", 100)
+console.log(eventObj3);
