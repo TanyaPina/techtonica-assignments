5f255477d26773c4c46a8f2323ab4a43af0bb3db
Tanya Pina <tanyapina15@gmail.com>
2023-02-10 13:54:26 -0800

    resolved heads spinning animation bug

--- coin-flip/coin-flip.js
+++ coin-flip/coin-flip.js
@@ -1,24 +1,24 @@
 const coin = document.getElementsByClassName("coin");
 const flipButton = document.getElementById("flip");
 const resetButton = document.getElementById("reset");
-const headsParagraph = document.getElementsByClassName("heads");
-const tailsParagraph = document.getElementsByClassName("tails");
+//const headsSection = document.getElementsByClassName("heads");
+//const tailsSection = document.getElementsByClassName("tails");
 
 let heads = 0;
 let tails = 0;
 //sets values on scoreboard to 0
 
-flipButton.addEventListener("click", () => {
+flipButton.addEventListener("click", function(e) {
     //adding event listener when user clicks on flip button
     let i = Math.floor(Math.random() * 2);
     //seting a random number generator to determine heads or tails
     coin[0].style.animation = "none";
     //there is no animation in a default stage
-    if(i){
+    if(i === 1){
     //if i is not rounded to 0 and therefore true
     setTimeout(function() {
         //headsParagraph[0].style.animation = "spin-heads 3s forwards";
-        document.getElementById("heads").style.animation = "spin-heads 3s forwards";
+        document.getElementById("coin").style.animation = "spin-heads 3s forwards";
         }, 100);
         heads++;  
     // it is heads, spin coin for 3 seconds forwards and add a point to 
@@ -27,7 +27,7 @@ flipButton.addEventListener("click", () => {
     else{
     //if i is rounded to 0
     setTimeout(function() {
-        document.getElementById("tails").style.animation = "spin-tails 3s forwards";
+        document.getElementById("coin").style.animation = "spin-tails 3s forwards";
         }, 100);
         tails++;
     // it is tails, spin coin for 3 seconds forwards and add a point to 
