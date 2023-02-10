4a8175fa7a29fbcc68d6dca34ed3c3096b9991cc
Tanya Pina <tanyapina15@gmail.com>
2023-02-09 23:18:41 -0800

    set default scoreboard values

--- coin-flip/coin-flip.js
+++ coin-flip/coin-flip.js
@@ -2,3 +2,6 @@ const coin = document.getElementsByClassName("coin");
 const flipButton = document.getElementsById("flip");
 const resetButton = document.getElementsById("reset");
 
+let heads = 0;
+let tails = 0;
+//sets values on scoreboard to 0
