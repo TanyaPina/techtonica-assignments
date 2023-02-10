be00b2c959afd8646e0d62d69388165feedca7f7
Tanya Pina <tanyapina15@gmail.com>
2023-02-10 01:41:17 -0800

    added function to update the scoreboard

--- coin-flip/coin-flip.js
+++ coin-flip/coin-flip.js
@@ -1,21 +1,24 @@
 const coin = document.getElementsByClassName("coin");
-const flipButton = document.getElementsById("flip");
-const resetButton = document.getElementsById("reset");
+const flipButton = document.getElementById("flip");
+const resetButton = document.getElementById("reset");
+const headsParagraph = document.getElementsByClassName("heads");
+const tailsParagraph = document.getElementsByClassName("tails");
 
 let heads = 0;
 let tails = 0;
 //sets values on scoreboard to 0
 
-flipButton.addEventListener("onClick", () => {
+flipButton.addEventListener("click", () => {
     //adding event listener when user clicks on flip button
     let i = Math.floor(Math.random() * 2);
     //seting a random number generator to determine heads or tails
-    coin.style.animation = "none";
+    coin[0].style.animation = "none";
     //there is no animation in a default stage
     if(i){
     //if i is not rounded to 0 and therefore true
     setTimeout(function() {
-        coin.style.animation = "spin-heads 3s forwards";
+        //headsParagraph[0].style.animation = "spin-heads 3s forwards";
+        document.getElementById("heads").style.animation = "spin-heads 3s forwards";
         }, 100);
         heads++;  
     // it is heads, spin coin for 3 seconds forwards and add a point to 
@@ -24,12 +27,21 @@ flipButton.addEventListener("onClick", () => {
     else{
     //if i is rounded to 0
     setTimeout(function() {
-        coin.style.animation = "spin-tails 3s forwards";
+        document.getElementById("tails").style.animation = "spin-tails 3s forwards";
         }, 100);
         tails++;
     // it is tails, spin coin for 3 seconds forwards and add a point to 
     //tails counter on scoreboard. Timeout action at 100 millliseconds.     
     }
-    setTimeout(updateStats, 3000);
-    //this event will tiemout and update stats at 3000 milliseconds.
-})
+    setTimeout(updateScoreboard, 3000);
+    //this event will timeout and update stats at 3000 milliseconds.
+    disableButton();
+});
+
+const headsCounter = document.getElementById("heads-count");
+const tailsCounter = document.getElementById("tails-count");
+function updateScoreboard(){
+    headsCounter.innerHTML = `Heads: ${heads}`;
+    tailsCounter.innerHTML = `Tails: ${tails}`;
+}
+
