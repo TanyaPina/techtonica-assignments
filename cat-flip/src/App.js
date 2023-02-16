762502ad5010e37f5c6094468fe09d877bcca47c
Tanya Pina <tanyapina15@gmail.com>
2023-02-15 22:04:41 -0800

    added tails div with image

--- cat-flip/src/App.js
+++ cat-flip/src/App.js
@@ -9,6 +9,10 @@ function App() {
         <img src="https://i.pinimg.com/564x/26/89/12/268912fec7d45b4ba9dfafb1046d7b1f.jpg" 
         alt="black silhoutte of a cat head"/>
         </div>
+        <div id="tails">
+        <img src="https://i.pinimg.com/564x/eb/06/2f/eb062f73192a0e44871b4085547158c6.jpg" 
+        alt="black silhoutte of a cat tail"/>
+        </div>
 
       </section>
         <img src={logo} className="App-logo" alt="logo" />
