7aa5382e8f31b7b0919f1baf4cd038a7aa9fbb0f
Tanya Pina <tanyapina15@gmail.com>
2023-02-15 22:00:00 -0800

    added heads div with image

--- cat-flip/src/App.js
+++ cat-flip/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <section className="catflip-grid">
       <section id="coin">
+        <div id="heads">
+        <img src="https://i.pinimg.com/564x/26/89/12/268912fec7d45b4ba9dfafb1046d7b1f.jpg" 
+        alt="black silhoutte of a cat head"/>
+        </div>
 
       </section>
         <img src={logo} className="App-logo" alt="logo" />
