6efcaf4828ca14982cecc99e23397606a322b154
Tanya Pina <tanyapina15@gmail.com>
2023-02-24 00:09:35 -0800

    added get route to zines

--- rest-api-project/index.js
+++ rest-api-project/index.js
@@ -9,6 +9,11 @@ app.use(cors());
 app.get("/", (req,res) => {
     res.json("Hello, my name is 5001 and I will be your server this evening.");
 })
+
+app.get("/api/zines", (req,res) => {
+    res.json(zines);
+})
+
 const PORT = 5001; 
 
 app.listen(PORT, () => console.log(`Ahoy matey! Server is running on port ${PORT}`));
