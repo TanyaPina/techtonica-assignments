9597f785714273c0287f756a4ae2e13bc6020f43
Tanya Pina <tanyapina15@gmail.com>
2023-02-24 00:00:24 -0800

    added homepage route

--- rest-api-project/index.js
+++ rest-api-project/index.js
@@ -5,6 +5,10 @@ const zines = require("./zines.js");
 const app = express();
 
 app.use(cors());
+
+app.get("/", (req,res) => {
+    res.json("Hello, my name is 5001 and I will be your server this evening.");
+})
 const PORT = 5001; 
 
 app.listen(PORT, () => console.log(`Ahoy matey! Server is running on port ${PORT}`));
