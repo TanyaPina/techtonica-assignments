c6116edba887109827da8d57767019e5c9890779
Tanya Pina <tanyapina15@gmail.com>
2023-03-03 12:19:31 -0800

    updated feels like props

--- weather-map-wk7/client/src/components/weatherCard.js
+++ weather-map-wk7/client/src/components/weatherCard.js
@@ -5,14 +5,14 @@ const WeatherCard = (props) => {
         <div className="weather-card">
             <div className="result">
                 <p>City: <span className="data">
-                    {props.data.weather[0].name}, {props.data.weather[0].sys.country}
+                    {data.name}, {data.sys.country}
                 </span>
                 </p>
                 <p>Description: <span className="data">
-                {props.data.weather[0].description}
+                {data.weather[0].description}
               </span>
             </p>
-            <img src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png`} alt={"Icon from Open Weather Api"}/>
+            <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`} alt={"Icon from Open Weather Api"}/>
             <p>
               Temperature: <span className="data">
                 {props.data.weather[0].main.temp} <sup>o</sup>F
@@ -20,7 +20,7 @@ const WeatherCard = (props) => {
             </p>
             <p>
               Feels Like: <span className="data">
-                {props.data.weather[0].main.feels_like} <sup>o</sup>F
+                {data.main.feels_like} <sup>o</sup>F
               </span>
             </p>
 
