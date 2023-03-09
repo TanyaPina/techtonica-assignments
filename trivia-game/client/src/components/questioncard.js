24dee7d2ae7f3cd1b4414c1bb9682f56c889c06e
Tanya Pina <tanyapina15@gmail.com>
2023-03-08 23:12:16 -0800

    added onClick functions to buttons

--- trivia-game/client/src/components/questioncard.js
+++ trivia-game/client/src/components/questioncard.js
@@ -1,13 +1,28 @@
 import {decode} from 'html-entities';
+import { useState} from "react";
 
 const QuestionCard = (props) => {
+    const [answered, setAnswered] = useState(false)
+    const [score,setScore] = useState(0);
     
+const handleAnswerOptionClick = (correct_answer) => {
+  if (correct_answer) {
+    setScore(score + 1);
+    setAnswered(true);
+  } else {
+    setAnswered(true);
+  }
+};
     return (
       <div className={"question-section"}>
+        <div>Question {props.questions}</div>
         <div className='question-text'>{decode(props.question.question)}</div>
         <div className='answer-section'>
-		<button>True</button>
-		<button>False</button>
+		<button disabled={answered} onClick= {() => handleAnswerOptionClick(props.question.question.correct_answer)} >True</button>
+		<button disabled={answered} onClick= {() => handleAnswerOptionClick(props.question.question.correct_answer)}>False</button>
+   {/* {question.correct_answer.map((answerOption)=>
+      <button onClick={() =>}
+   )}*/}
         </div>
       </div>
     );
